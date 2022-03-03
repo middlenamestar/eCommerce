38ab77d2ded728848eaa1007f3ff3c62008b30c8
priscilla ma <priscillama621@gmail.com>
2022-03-02 21:49:54 -0500

    forgot res json

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -9,6 +9,7 @@ router.get('/', async (req, res) => {
     const tags = await Tag.findAll({
       include: Product
     });
+    res.status(200).json(tags);
   } catch (err) {
     res.status(500).json(err);
   }
